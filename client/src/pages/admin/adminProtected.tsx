989465898a3b2c09069e516e17467dfb06047487
Ubaida Abdul <ubaidaabdul723@gmail.com>
2026-01-17 13:41:51 +0000

    small change

--- client/src/pages/admin/adminProtected.tsx
+++ client/src/pages/admin/adminProtected.tsx
@@ -1,7 +1,7 @@
 import { Redirect } from "wouter";
 
 export default function AdminProtectedRoute({ children }: any) {
-  const token = localStorage.getItem("adminToken");
+  const token = localStorage.getItem("admin_access_token");
 
   if (!token) {
     return <Redirect to="/admin/login" />;
